e7ff66b2283f10f5baf65bc1eae4863a9324ac65
Soy Adri <adriisempere@hotmail.com>
2025-12-16 09:25:22 +0100

    cambio ruta

--- app/views/home.php
+++ app/views/home.php
@@ -6,7 +6,7 @@
     <title>GreenPoints | Recicla y Gana</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="../public/css/estilos.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
 </head>
 <body>
