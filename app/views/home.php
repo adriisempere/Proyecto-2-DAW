f6251bc2edba36a6294f595dad710f5bac8fbdb9
Soy Adri <adriisempere@hotmail.com>
2026-01-13 08:53:50 +0100

    Update home.php

--- app/views/home.php
+++ app/views/home.php
@@ -32,8 +32,29 @@
     </style>
 <body>
 
-    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="main-nav">
-        
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">GreenPoints</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Ranking</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Puntos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Tienda</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Mi Perfil</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </nav>
 
     <header class="hero d-flex align-items-center text-center text-white" id="inicio">
